e091f3a0c79a106b4c4e9f9ab53208baadd18df9
Elif Sanem Ceyhan <270886456+sanerifu@users.noreply.github.com>
2026-06-08 22:13:23 +0300

    Add documentation

--- args.h
+++ args.h
@@ -70,6 +70,28 @@
     }
 #define ___ESTD_ARGS_OPTIONAL_POSITIONAL_CHECKER_SEPARATOR else
 
+/**
+ * @brief Generator macro for argument parser for given argument handlers
+ * @details This uses X-macro concept. Each argument declaration must be a macro taking two arguments: the per-argument handler and separator
+ * The separator is just a macro, it is used as-is. It must be interleaved between argument declarations The per-argument handler is another function taking 3 arguments.
+ * 1. The identifier of the argument. Must be a valid C identifier. Can start with number. For named arguments, it is checked as `--id` where `id` is the identifier.
+ * Positional arguments use the name only for information for help message and errors. Still, they must obey same rules to be valid.
+ * 2. The description of the argument. This must be a string literal It is used only for help message.
+ * 3. The action for the argument. This must be enclosed in parantheses. It can be any statement. Named arguments have `value` defined as the value of the argument.
+ * Positional arguments have `arg` defined as the argument itself. Both are `EstdString`s.
+ * The resulting parser accepts first only named arguments, then only positional arguments. Named arguments always start with `--`, followed by
+ * identifier, followed by `=` and the value. The `=` and value are optional, if they don't exist the `value` variable will simply be empty.
+ * For example: `./program --flag1 --output=foo.txt --input=bar.txt compile` will apply actions of `flag1` (with empty value), `output` (with `foo.txt` value),
+ * `input` (with `bar.txt` value) and the first positional argument (with `compile` arg).
+ * @param name Name of the parser function to be generated
+ * @param params Extra parameters passed to the parser function. Must have at least one parameter. Must be enclosed in parantheses
+ * @example
+ * #define EXAMPLE_NAMED_ARGS(ARG, SEP) ARG(foo, "This is foo's description", (do_something_with_foo(value))) SEP ARG(bar, "This is bar's description", ())
+ * #define EXAMPLE_POS_ARGS(ARG, SEP) ARG(baz, "Baz, this is", (printf("Baz is %" PRIestr "\n", ESTD_STRING_ARG(arg)))) SEP ARG(another, "", ())
+ * ESTD_GENERATE_ARGUMENT_PARSER(my_parser_fun, (int), EXAMPLE_NAMED_ARGS, ESTD_NO_ARGUMENT, ESTD_NO_ARGUMENT, EXAMPLE_POS_ARGS)
+ * // which generates `my_parser_fun`, taking argc, argv and unnamed int. Can be used like:
+ * ESTD_BUBBLE(my_parser_fun(argc, argv, 0), "Could not parse command line arguments");
+ */
 #define ESTD_GENERATE_ARGUMENT_PARSER(                                                                                                                          \
     name,                                                                                                                                                       \
     params,                                                                                                                                                     \
