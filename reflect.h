17e8b7d275a1012064959c1c1b7c514185586523
Elif Sanem Ceyhan <270886456+sanerifu@users.noreply.github.com>
2026-07-22 22:12:47 +0300

    Add generic constructor

--- reflect.h
+++ reflect.h
@@ -14,6 +14,11 @@
     }
 #define ___ESTD_UNION_CONSTRUCTOR_SEP
 
+#define ___ESTD_UNION_GENERIC_CONSTRUCTOR_CASE(ctx, T, name) \
+    T:                                                       \
+    name##ctx
+#define ___ESTD_UNION_GENERIC_CONSTRUCTOR_SEP ,
+
 #define ESTD_DECLARE_UNION(T)                                                                             \
     typedef enum ___##T##Type{T##Cases(T, ___ESTD_UNION_ENUM_CASE, ___ESTD_UNION_ENUM_SEP)} ___##T##Type; \
     typedef union ___##T##Data {                                                                          \
@@ -24,5 +29,8 @@
         ___##T##Data ___data;                                                                             \
     } T;                                                                                                  \
     T##Cases(T, ___ESTD_UNION_CONSTRUCTOR_CASE, ___ESTD_UNION_CONSTRUCTOR_SEP)
+
+#define ESTD_GENERIC_CONSTRUCTOR(T, value) \
+    _Generic((value), T##Cases(T, ___ESTD_UNION_GENERIC_CONSTRUCTOR_CASE, ___ESTD_UNION_GENERIC_CONSTRUCTOR_SEP))(value)
 
 #endif
